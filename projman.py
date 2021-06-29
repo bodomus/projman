95f0e8a687d8b8bd67fa68f55b0b572cf2990ac9
Andrey Petrov <andreyp@archer-soft.com>
2021-06-29 09:15:49 +0300

    ~refactor

--- projman.py
+++ projman.py
@@ -2,7 +2,8 @@ import hou
 import os
 from PySide2 import QtWidgets, QtUiTools
 
-class ProjectManager (QtWidgets.QWidget):
+
+class ProjectManager(QtWidgets.QWidget):
     def __init__(self):
         super(ProjectManager, self).__init__()
         self.job = hou.getenv("JOB")
@@ -10,12 +11,12 @@ class ProjectManager (QtWidgets.QWidget):
 
         loader = QtUiTools.QUiLoader()
         p = self.getUiPath()
-        self.ui = loader.load(r'c:/Houdini/17.5.327/houdini/python2.7libs/projman/projman.ui')
+        self.ui = loader.load(r'c:/Houdini/18.0.348/houdini/python2.7libs/projman/projman.ui')
         self.clsScene = self.ui.findChild(QtWidgets.QPushButton, "btnClearScene")
         self.clsScene.clicked.connect(self.clearScene)
         self.setPrj = self.ui.findChild(QtWidgets.QPushButton, "btnSetProject")
         self.setPrj.clicked.connect(self.setProject)
-        self.lblProjName  = self.ui.findChild(QtWidgets.QLabel, "lblProjectName")
+        self.lblProjName = self.ui.findChild(QtWidgets.QLabel, "lblProjectName")
         self.lblProjName.setText('')
         self.lblProjPath = self.ui.findChild(QtWidgets.QLabel, "lblProjectPath")
         self.lblProjPath.setText('')
@@ -37,15 +38,15 @@ class ProjectManager (QtWidgets.QWidget):
 
     def setProject(self):
         job = hou.ui.selectFile(title='Select directory', file_type=hou.fileType.Directory)
-        hou.hscript("setenv JOB="+job)
+        hou.hscript("setenv JOB=" + job)
         self.lblProjName.setText(job.split('/')[-2])
         self.lblProjPath.setText(hou.expandString(job))
         self.createInterface()
 
     def createInterface(self):
-        #self.list.clear()
+        # self.list.clear()
         job = hou.getenv('JOB')
-        #job=job.replace('/','\\')
+        # job=job.replace('/','\\')
         try:
             for file in os.listdir(job):
                 if file.endswith('.hip'):
@@ -56,5 +57,4 @@ class ProjectManager (QtWidgets.QWidget):
         return None
 
     def getUiPath(self):
-        return hou.getenv('HFS')+'/houdini/python2.7libs/projman/'
-
+        return hou.getenv('HFS') + '/houdini/python2.7libs/projman/'
